22a474274c295ddc02584b30c785507c8de12d45
Isaac Saffran <Isaac.Saffran@edu.sait.ca>
2023-03-22 22:07:48 -0600

    Add project files.

---
+++ problemdomain/User.cs
@@ -0,0 +1,27 @@
+﻿namespace ProblemDomain
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public User(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            User other = (User)obj;
+            return FirstName == other.FirstName && LastName == other.LastName;
+        }
+
+        // Consider overriding GetHashCode as well if needed
+    }
+}
---
+++ utility/ILinkedListADT.cs
@@ -0,0 +1,16 @@
+﻿namespace Utility
+{
+    public interface ILinkedListADT<T>
+    {
+        void Prepend(T data);
+        void Append(T data);
+        void InsertAt(T data, int index);
+        void Set(T data, int index);
+        T Get(int index);
+        int IndexOf(T data);
+        bool Contains(T data);
+        void Clear();
+        int Size();
+        T RemoveAt(int index);
+    }
+}
