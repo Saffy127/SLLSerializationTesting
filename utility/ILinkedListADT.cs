a5d325fafb4183b16bca7a79ee887654799d49c9
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-04-01 14:46:18 -0600

    Implementation of the SLL class
    Based on requirments specified by the ILinkedListADT interface.

--- utility/ILinkedListADT.cs
+++ utility/ILinkedListADT.cs
@@ -1,16 +1,160 @@
-﻿namespace Utility
+using System;
+
+namespace Utility
 {
-    public interface ILinkedListADT<T>
+    public class SLL<T> : ILinkedListADT<T>
     {
-        void Prepend(T data);
-        void Append(T data);
-        void InsertAt(T data, int index);
-        void Set(T data, int index);
-        T Get(int index);
-        int IndexOf(T data);
-        bool Contains(T data);
-        void Clear();
-        int Size();
-        T RemoveAt(int index);
+        private Node<T> head;
+        private int count;
+
+        public void Prepend(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            newNode.Next = head;
+            head = newNode;
+            count++;
+        }
+
+        public void Append(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (IsEmpty())
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node<T> current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+            count++;
+        }
+
+        public void InsertAt(T data, int index)
+        {
+            if (index < 0 || index > count)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index.");
+            }
+
+            if (index == 0)
+            {
+                Prepend(data);
+                return;
+            }
+
+            Node<T> newNode = new Node<T>(data);
+            Node<T> current = head;
+
+            for (int i = 0; i < index - 1; i++)
+            {
+                current = current.Next;
+            }
+
+            newNode.Next = current.Next;
+            current.Next = newNode;
+            count++;
+        }
+
+        public void Set(T data, int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index.");
+            }
+
+            Node<T> current = head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            current.Data = data;
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index.");
+            }
+
+            Node<T> current = head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            return current.Data;
+        }
+
+        public int IndexOf(T data)
+        {
+            int index = 0;
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    return index;
+                }
+                index++;
+                current = current.Next;
+            }
+
+            return -1;
+        }
+
+        public bool Contains(T data)
+        {
+            return IndexOf(data) != -1;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public int Size()
+        {
+            return count;
+        }
+
+        public T RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new ArgumentOutOfRangeException("Invalid index.");
+            }
+
+            if (index == 0)
+            {
+                T removedData = head.Data;
+                head = head.Next;
+                count--;
+                return removedData;
+            }
+
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++)
+            {
+                current = current.Next;
+            }
+
+            T removedData = current.Next.Data;
+            current.Next = current.Next.Next;
+            count--;
+            return removedData;
+        }
+
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
     }
 }
