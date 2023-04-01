33ddd7af8c8337baebe4cc5973db8fbcb5144d99
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-04-01 14:42:42 -0600

    serialize and deserialize a SLL object co

--- SLLSerializationTesting/Program.cs
+++ SLLSerializationTesting/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Utility;
+using ProblemDomain;
+using System.Text.Json;
+
+namespace SLLSerializationTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new singly linked list (SLL) of User objects
+            SLL<User> userList = new SLL<User>();
+            userList.Append(new User("John", "Doe"));
+            userList.Append(new User("Jane", "Doe"));
+            userList.Append(new User("Alice", "Smith"));
+
+            // Serialize the SLL object
+            MemoryStream memoryStream = new MemoryStream();
+            JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+            byte[] serializedData = JsonSerializer.SerializeToUtf8Bytes(userList, options);
+
+            // Deserialize the SLL object
+            MemoryStream deserializationStream = new MemoryStream(serializedData);
+            SLL<User> deserializedList = JsonSerializer.Deserialize<SLL<User>>(serializedData);
+
+            // Display the deserialized SLL object
+            for (int i = 0; i < deserializedList.Size(); i++)
+            {
+                User user = deserializedList.Get(i);
+                Console.WriteLine($"{user.FirstName} {user.LastName}");
+            }
+        }
+    }
+}
