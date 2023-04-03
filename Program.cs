337ae59375993d8b1e2dfb55b715ebf5a84f4838
Isaac Saffran <88598834+Saffy127@users.noreply.github.com>
2023-04-03 16:58:06 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Assignment_3_skeleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the SLL class
+            LinkedListADT userList = new SLL();
+
+            // Add Users to the list
+            userList.Append(new User(1, "John", "Doe", "john.doe@email.com"));
+            userList.Append(new User(2, "Jane", "Doe", "jane.doe@email.com"));
+            userList.Append(new User(3, "Jim", "Smith", "jim.smith@email.com"));
+
+            // Display the list
+            Console.WriteLine("List of Users:");
+            for (int i = 0; i < userList.Size(); i++)
+            {
+                Console.WriteLine($"{i + 1}. {userList.Retrieve(i)}");
+            }
+
+            // Run the provided test cases
+            Console.WriteLine("\nRunning Test Cases:");
+            LinkedListTest(userList);
+            SerializationTests(userList);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        static void LinkedListTest(LinkedListADT userList)
+        {
+            // Add your linked list tests here
+        }
+
+        static void SerializationTests(LinkedListADT userList)
+        {
+            // Add your serialization tests here
+        }
+    }
+}
